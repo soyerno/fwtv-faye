3951980c26b1fc8ce603c2d909d7ec0bbafbd948
fansworld-claudio <cr@fansworld.tv>
2014-07-31 10:02:44 -0300

    auth

--- web.js
+++ web.js
@@ -4,5 +4,30 @@ var http = require('http'),
 var server = http.createServer(),
   bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
+var authorized = function(message) {
+  if (message.hmac === process.env.FAYE_KEY) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+server.addExtension({
+  incoming: function(message, callback) {
+    if (message.channel !== '/meta/subscribe') {
+      if (!authorized(message)) {
+        message.error = '403::Authentication required';
+      } else {
+        if (message.hmac) {
+          delete message.hmac;
+        }
+        callback(message);
+      }
+    } else {
+      callback(message);
+    }
+  }
+});
+
 bayeux.attach(server);
 server.listen(process.env.PORT || 8000);
